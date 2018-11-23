604da3e4a695dbab29a43810ba2705c4208ed22d
lmnsyunhao <39151513+lmnsyunhao@users.noreply.github.com>
2018-11-23 16:28:18 +0800

    Functional Fixed: Error Code 429 From Leancloud (#11)
    * functional fixed: error code 429 from leancloud
    
    * rm the mark in src/index.js file
    
    * change readme.md

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var _stringify2 = _interopRequireDefault(_stringify);
 
 var sync = function () {
     var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee() {
-        var log, config, APP_ID, APP_KEY, publicDir, UrlsFile, urls, currentUser, userName, passWord, Counter;
+        var log, config, APP_ID, APP_KEY, publicDir, UrlsFile, urls, currentUser, userName, passWord, Counter, memoFile, memoData, memoIdx, newData, cnt, limit, env;
         return _regenerator2.default.wrap(function _callee$(_context) {
             while (1) {
                 switch (_context.prev = _context.next) {
@@ -23,7 +23,7 @@ var sync = function () {
                         config = this.config;
 
                         if (!config.leancloud_counter_security.enable_sync) {
-                            _context.next = 19;
+                            _context.next = 28;
                             break;
                         }
 
@@ -67,27 +67,69 @@ var sync = function () {
                         log.info('Now syncing your posts list to leancloud counter...');
                         Counter = AV.Object.extend('Counter');
 
+                        urls.sort(cmp);
+                        memoFile = pathFn.join(publicDir, "leancloud.memo");
+
+                        if (!fs.existsSync(memoFile)) {
+                            fs.writeFileSync(memoFile, "[\n]");
+                        }
+                        memoData = fs.readFileSync(memoFile, "utf-8").split("\n");
+                        memoIdx = 1;
+                        newData = [];
+                        cnt = 0;
+                        limit = 0;
+                        env = this;
+
                         _.forEach(urls, function (x) {
-                            var query = new AV.Query('Counter');
-                            query.equalTo('url', x.url);
-                            query.count().then(function (count) {
-                                if (count === 0) {
-                                    var counter = new Counter();
-                                    counter.set('url', x.url);
-                                    counter.set('title', x.title);
-                                    counter.set('time', 0);
-                                    counter.save().then(function (obj) {
-                                        log.info(x.title + ' is saved as: ' + obj.id);
-                                    }, function (error) {
-                                        log.error(error);
-                                    });
+                            var y = {};
+                            y.title = "";
+                            y.url = "";
+                            var flag = false;
+                            while (true) {
+                                if (memoData[memoIdx] == ']') break;
+                                y = JSON.parse(memoData[memoIdx].substring(0, memoData[memoIdx].length - 1));
+                                if (y.url > x.url) break;
+                                if (y.url == x.url && y.title == x.title) {
+                                    flag = true;
+                                    break;
                                 }
-                            }, function (error) {
-                                log.error(error);
-                            });
+                                memoIdx++;
+                            }
+                            if (!flag) {
+                                log.info("Dealing with record of " + x.title);
+                                limit++;
+                                var query = new AV.Query('Counter');
+                                query.equalTo('url', x.url);
+                                query.count().then(function (count) {
+                                    if (count === 0) {
+                                        var counter = new Counter();
+                                        counter.set('url', x.url);
+                                        counter.set('title', x.title);
+                                        counter.set('time', 0);
+                                        counter.save().then(function (obj) {
+                                            log.info(x.title + ' is saved as: ' + obj.id);
+                                            newData.push(x);
+                                            cnt++;
+                                            postOperation(env, cnt, limit, newData, memoData);
+                                        }, function (error) {
+                                            log.error(error);
+                                            cnt++;
+                                            postOperation(env, cnt, limit, newData, memoData);
+                                        });
+                                    } else {
+                                        newData.push(x);
+                                        cnt++;
+                                        postOperation(env, cnt, limit, newData, memoData);
+                                    }
+                                }, function (error) {
+                                    log.error(error);
+                                    cnt++;
+                                    postOperation(env, cnt, limit, newData, memoData);
+                                });
+                            }
                         });
 
-                    case 19:
+                    case 28:
                     case 'end':
                         return _context.stop();
                 }
@@ -169,3 +211,43 @@ function commandFunc(args) {
 }
 
 hexo.extend.console.register('lc-counter', 'hexo-leancloud-counter-security', commandOptions, commandFunc);
+
+function cmp(x, y) {
+    if (x.url < y.url) return -1;else if (x.url == y.url) return 0;else return 1;
+}
+
+var postOperation = function postOperation(env, cnt, limit, newData, memoData) {
+    if (cnt == limit) {
+        var log = env.log;
+        newData.sort(cmp);
+        var sourceDir = env.source_dir;
+        var publicDir = env.public_dir;
+        var memoFile = pathFn.join(sourceDir, "leancloud.memo");
+        fs.writeFileSync(memoFile, "[\n");
+
+        var memoIdx = 1;
+        for (var i = 0; newData[i]; i++) {
+            while (true) {
+                if (memoData[memoIdx] == ']') break;
+                var y = JSON.parse(memoData[memoIdx].substring(0, memoData[memoIdx].length - 1));
+                if (y.url > newData[i].url) break;
+
+                fs.writeFileSync(memoFile, memoData[memoIdx] + "\n", { 'flag': 'a' });
+                memoIdx++;
+            }
+            fs.writeFileSync(memoFile, "{\"title\":\"" + newData[i].title + "\",\"url\":\"" + newData[i].url + "\"},\n", { 'flag': 'a' });
+        }
+        while (memoData[memoIdx] != ']') {
+            fs.writeFileSync(memoFile, memoData[memoIdx] + "\n", { 'flag': 'a' });
+            memoIdx++;
+        }
+        fs.writeFileSync(memoFile, memoData[memoIdx], { 'flag': 'a' });
+
+        var srcFile = pathFn.join(sourceDir, "leancloud.memo");
+        var destFile = pathFn.join(publicDir, "leancloud.memo");
+        var readStream = fs.createReadStream(srcFile);
+        var writeStream = fs.createWriteStream(destFile);
+        readStream.pipe(writeStream);
+        console.log("leancloud.memo successfully updated.");
+    }
+};
